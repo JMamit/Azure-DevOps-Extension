022ef5e5a24caa9547b91a3fafd5bdaf7c226135
NotASpamBot16 <70576717+NotASpamBot16@users.noreply.github.com>
2021-06-22 23:06:39 +0530

    commit

---
+++ javaScript/CDN2.js
@@ -0,0 +1,1 @@
+if(typeof Msal === 'undefined')document.write(unescape("%3Cscript src='https://alcdn.msftauth.net/browser/2.12.1/js/msal-browser.js' type='text/javascript' crossorigin='anonymous' %3E%3C/script%3E"));
